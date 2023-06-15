df04840aec525ee592116ebdcf1b19a07e73147f
KuroNekoLi <alice.margatroid.love@gmail.com>
2023-06-15 11:50:55 +0800

    新增獲取work的狀態

--- app/src/main/java/com/example/workmanagerdemo1/MainActivity.kt
+++ app/src/main/java/com/example/workmanagerdemo1/MainActivity.kt
@@ -3,23 +3,33 @@ package com.example.workmanagerdemo1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btnStart = findViewById<Button>(R.id.btn_start)
+        textView = findViewById(R.id.textView)
         btnStart.setOnClickListener { setOneTimeWorkRequest() }
     }
 
     private fun setOneTimeWorkRequest(){
-//        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+        val workManager = WorkManager.getInstance(applicationContext)
+//        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
 //            .build()
-        val uploadWorkRequest = OneTimeWorkRequestBuilder<UploadWorker>().build()
-        WorkManager.getInstance(applicationContext).enqueue(uploadWorkRequest)
+        val uploadRequest = OneTimeWorkRequestBuilder<UploadWorker>().build()
+        workManager.enqueue(uploadRequest)
+        workManager.getWorkInfoByIdLiveData(uploadRequest.id)
+            .observe(this, Observer {
+                textView.text = it.state.name //有四種狀態SUCCEEDED RUNNING ENQUEUED BLOCKED
+            })
     }
 
 
--- app/src/main/java/com/example/workmanagerdemo1/UploadWorker.kt
+++ app/src/main/java/com/example/workmanagerdemo1/UploadWorker.kt
@@ -1,6 +1,7 @@
 package com.example.workmanagerdemo1
 
 import android.content.Context
+import android.os.SystemClock
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
@@ -11,6 +12,7 @@ class UploadWorker(context: Context,params:WorkerParameters) : Worker(context,pa
         try {
             for (i in 0..600){
                 Log.i(TAG, "Uploading $i ")
+//                SystemClock.sleep(180*1000) //增加此行可以看到Running狀態
             }
             return Result.success()
         } catch (e: Exception) {
